0d83f0760c2154c62a6132417d2779a6b46fd9a7
Lukas Winiwarter <lukas.winiwarter@ubc.ca>
2022-11-29 12:31:48 -0800

    updated mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 
 autodoc_mock_imports = ['xarray', 'pandas', 'numpy',
                         'scipy', 'laxpy', 'tqdm',
-                        'matplotlib', 'shapely']
+                        'matplotlib', 'shapely', 'deprecated']
 # -- Project information
 
 from sphinx_pyproject import SphinxConfig
